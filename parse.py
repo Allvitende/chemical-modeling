7a1c20edbad7e74422457bb77ca4a1d2f7ffa563
Allvitende <broschroeder@gmail.com>
2017-06-12 17:07:45 -0500

    Add functionality to parse connection table into the connection table for all Atomsls

--- parse.py
+++ parse.py
@@ -1,8 +1,8 @@
 from periodictable import elements
 from scipy.spatial import distance
 
-global filename = 'train.sdf'
-global startstr = "  -OEChem"
+filename = 'train.sdf'
+startstr = "  -OEChem"
 
 def get_dist(l, xyz, numxyz):
     distlist = []
@@ -39,9 +39,11 @@ for idx, line in enumerate(data) :
 
         xyz = [[0.0 for i in range(3)] for j in range (numxyz)]
         connect = [[0 for i in range(3)] for j in range(numc)]
+        connection_table = [[0 for i in range(numxyz)] for j in range (numxyz)]
         atmnum = [0 for i in range(numxyz)]
+        distance_table = []
         # mergedlist = []
-        dists = []
+
         for i in range(numxyz) :
             temp = data[idx + 3 + i].split()
             # Loop through periodic table to find element number for each symbol and store it
@@ -51,27 +53,41 @@ for idx, line in enumerate(data) :
             for j in range(3) :
                 xyz[i][j] = float(temp[j])
 
+        # TEST CODE
         # for i in range(numxyz): print('{:d} {:s} {:12.5f} {:s} {:12.5f} {:s} {:12.5f}' .format( atmnum[i], ', ', xyz[i][0], ', ', xyz[i][1], ', ',xyz[i][2]) )
         # for i in range(numxyz):
         #     mergedlist.append([atmnum[i], xyz[i][0], xyz[i][1], xyz[i][2]])
         # print(mergedlist[0])
 
         # Build the distance matrix for each atom in the molecule
-        for l in range(numxyz):
-            dists.append(get_dist(l, xyz, numxyz))
+        # for l in range(numxyz):
+        #     distsance_table.append(get_dist(l, xyz, numxyz))
+        # TEST CODE
         # print(dists)
-        # break
 
-        # for i in range(numc) :
-        #     temp = data[idx + 3 + i + numxyz].split()
-        #     if len(temp) == 2 :
-        #         atmcount = temp[0]
-        #         n = len(atmcount)
-        #         connect[i][0] = int(atmcount[0:n/2])
-        #         connect[i][1] = int(atmcount[n/2:n])
-        #         connect[i][2] = int(temp[1])
-        #     else :
-        #         connect[i][0] = int(temp[0])
-        #         connect[i][1] = int(temp[1])
-        #         connect[i][2] = int(temp[2])
+        for i in range(numc) :
+            temp = data[idx + 3 + i + numxyz].split()
+            if len(temp) == 2 :
+                atmcount = temp[0]
+                n = len(atmcount)
+                connect[i][0] = int(atmcount[0:n/2])
+                connect[i][1] = int(atmcount[n/2:n])
+                connect[i][2] = int(temp[1])
+            else :
+                connect[i][0] = int(temp[0])
+                connect[i][1] = int(temp[1])
+                connect[i][2] = int(temp[2])
+        # TEST CODE
         # for i in range(numc): print('{:5d} {:s} {:5d} {:s} {:5d}' .format( connect[i][0], ', ', connect[i][1], ', ',connect[i][2]) )
+
+        # Record connection information in the connection table of all Atoms in molecule
+        for i in range(numc):
+            # Only supports molecules with atom count < 100 will fix later...
+            if n < 3:
+                connection_table[connect[i][0] - 1][connect[i][1] - 1] = 1
+                connection_table[connect[i][1] - 1][connect[i][0] - 1] = 1
+
+        # TEST CODE
+        for i in range(numxyz): print('{:s}' .format(connection_table[i]))
+        # TEST CODE
+        # break
