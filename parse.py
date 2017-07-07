cdf33f8129c4c5b9065e6f1b5c70109169735682
Allvitende <broschroeder@gmail.com>
2017-07-07 17:53:39 -0500

    Refactor parsing functions. Implement NumPy library. Implement zero padding

--- parse.py
+++ parse.py
@@ -1,19 +1,20 @@
+import numpy as np
 from periodictable import elements
 from scipy.spatial import distance
 
 filename = 'train.sdf'
 startstr = "  -OEChem"
 
-def get_dist(l, xyz, numxyz):
-    distlist = []
-    for i in range(numxyz):
-        # Distance between an atom and itself is zero
-        if i == l:
-            distlist.extend([0])
-        else:
-            # Calculate distance between atoms and all the other vectors in the xyz matrix (need to optimize this)
-            distlist.extend([distance.euclidean(xyz[l], xyz[i])])
-    return distlist
+# def get_dist(l, xyz, numxyz):
+#     distlist = []
+#     for i in range(numxyz):
+#         # Distance between an atom and itself is zero
+#         if i == l:
+#             distlist.extend([0])
+#         else:
+#             # Calculate distance between atoms and all the other vectors in the xyz matrix (need to optimize this)
+#             distlist.append(distance.euclidean(xyz[l], xyz[i]))
+#     return distlist
 
 # Read entire file and parse into list of strings
 with open(filename, 'r') as f:
@@ -30,6 +31,9 @@ for idx, line in enumerate(data) :
         temp = data[idx + 2].split()
         # Store atom count
         atmcount = temp[0]
+        if int(atmcount) > 28:
+            print("Skipping this molecule")
+            continue
         n = len(atmcount)
         # Scenario if atom count is greater than 999
         if n > 3 :
@@ -38,13 +42,13 @@ for idx, line in enumerate(data) :
         else :
             numxyz = int(temp[0])
             numc = int(temp[1])
-        # print('{:s}{:4d}{:s}{:4d}'.format('number of atoms =', numxyz, ' number of connections =', numc))
+        print('{:s}{:4d}{:s}{:4d}'.format('number of atoms =', numxyz, ' number of connections =', numc))
 
         xyz = [[0.0 for i in range(3)] for j in range (numxyz)]
         connect = [[0 for i in range(3)] for j in range(numc)]
-        connection_table = [[0 for i in range(numxyz)] for j in range (numxyz)]
-        atmnum = [0 for i in range(numxyz)]
-        distance_table = []
+        atmnum = np.zeros(shape=(28,1))
+        connection_table = np.zeros(shape=(28,28))
+        distance_table = np.zeros(shape=(28,29))
         # mergedlist = []
 
         for i in range(numxyz) :
@@ -52,7 +56,7 @@ for idx, line in enumerate(data) :
             # Loop through periodic table to find element number for each symbol and store it
             for el in elements :
                 if temp[3] == el.symbol :
-                    atmnum[i] = el.number
+                    atmnum[i] = float(el.number)
             for j in range(3) :
                 xyz[i][j] = float(temp[j])
 
@@ -62,11 +66,16 @@ for idx, line in enumerate(data) :
         #     mergedlist.append([atmnum[i], xyz[i][0], xyz[i][1], xyz[i][2]])
         # print(mergedlist[0])
 
-        # Build the distance matrix for each atom in the molecule
+        for a in range(numxyz):
+            distance_table[a][0] = float(atmnum[a])
+        # print(distance_table)
+        # print(len(distance_table[a]))
         for l in range(numxyz):
-            distsance_table.append(get_dist(l, xyz, numxyz))
-        # TEST CODE
-        # print(dists)
+            for i in range(numxyz):
+                distance_table[l][i + 1] = distance.euclidean(xyz[l], xyz[i])
+        # # TEST CODE
+        print(distance_table.flatten()) # Prep for TF Model
+        print(len(distance_table.flatten())) # Should print 784
 
         for i in range(numc) :
             temp = data[idx + 3 + i + numxyz].split()
@@ -85,12 +94,12 @@ for idx, line in enumerate(data) :
 
         # Record connection information in the connection table of all Atoms in molecule
         for i in range(numc):
-            # Skips over a corrupt portion of this particular SDF file on line 655645
-            if n < 3:
-                connection_table[connect[i][0] - 1][connect[i][1] - 1] = 1
-                connection_table[connect[i][1] - 1][connect[i][0] - 1] = 1
+            connection_table[connect[i][0] - 1][connect[i][1] - 1] = 1.0
+            connection_table[connect[i][1] - 1][connect[i][0] - 1] = 1.0
 
+        atmnum.shape = (28, 1)
+        connection_table_final = np.hstack((atmnum, connection_table))
         # TEST CODE
-        # for i in range(numxyz): print('{:s}' .format(connection_table[i]))
+        print(connection_table_final.flatten())
+        #for i in range(28): print('{:s}' .format(connection_table[i]))
         # TEST CODE
-        # break
--- tf_chem.py
+++ tf_chem.py
@@ -1,18 +1,33 @@
 import tensorflow as tf
 
-# x will be the input matrix flattened (28x28)
-x = tf.placeholder(tf.float32, [None, 784])
+input_data =
+# x will be the input matrix flattened (28x29)
+x = tf.placeholder(tf.float32, [None, 812])
 
 # Define the weights (initial value doesn't matter since these will be learned)
-W = tf.Variable(tf.zeros([784, 784]))
-b = tf.Variable(tf.zeros([784]))
+W = tf.Variable(tf.zeros([812, 812]))
+b = tf.Variable(tf.zeros([812]))
 
 # Predict output matrix
 y = tf.nn.softmax(tf.matmul(x, W) + b)
 
 # Actual output matrix from the training set
-y_ = tf.placeholder(tf.float32, [None, 784])
+y_ = tf.placeholder(tf.float32, [None, 812])
 
 # Calculate loss and optimize
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
-train_step = tf.train.GradientDescentOptimizer(0.05).minimize(cross_entropy)
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run()
+
+# Training
+for _ in range(1000):
+    batch_xs, batch_ys = input_data.train.next_batch(100)
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+# Test trained model
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print(sess.run(accuracy, feed_dict={x: input_data.test.distance_table,
+                                    y_: input_data.test.connection_table}))
