ceefd0c19676dd662f61fca84b2604948417c3ad
Allvitende <broschroeder@gmail.com>
2017-07-25 11:44:02 -0500

    move parsing into a separate module

--- tf_chem.py
+++ tf_chem.py
@@ -1,84 +1,5 @@
-import numpy as np
 import tensorflow as tf
-from periodictable import elements
-from scipy.spatial import distance
-
-filename = 'train.sdf'
-startstr = "  -OEChem"
-
-# Read entire file and parse into list of strings
-with open(filename, 'r') as f:
-    data = f.readlines()
-# print( '{:d} {:s}'.format(len(data), ' lines read' ) )
-
-def get_batch():
-    train_data_dists = []
-    train_data_connects = []
-    # Search for the beginning of each block of Atoms in file and get the index
-    for idx, line in enumerate(data) :
-        if startstr in line :
-            # Store line 4 from each block into a list
-            temp = data[idx + 2].split()
-            # Store atom count
-            atmcount = temp[0]
-            if int(atmcount) > 28:
-                # print("Skipping this molecule")
-                continue
-            n = len(atmcount)
-            # Scenario if atom count is greater than 999
-            if n > 3 :
-                numxyz = int(atmcount[0:n/2])
-                numc = int(atmcount[n/2:n])
-            else :
-                numxyz = int(temp[0])
-                numc = int(temp[1])
-
-            xyz = [[0.0 for i in range(3)] for j in range (numxyz)]
-            connect = [[0 for i in range(3)] for j in range(numc)]
-            atmnum = np.zeros(shape=(28,1))
-            connection_table = np.zeros(shape=(28,28))
-            distance_table = np.zeros(shape=(28,29))
-
-            for i in range(numxyz) :
-                temp = data[idx + 3 + i].split()
-                # Loop through periodic table to find element number for each symbol and store it
-                for el in elements :
-                    if temp[3] == el.symbol :
-                        atmnum[i] = float(el.number)
-                for j in range(3) :
-                    xyz[i][j] = float(temp[j])
-
-            for a in range(numxyz):
-                distance_table[a][0] = float(atmnum[a])
-
-            for l in range(numxyz):
-                for i in range(numxyz):
-                    distance_table[l][i + 1] = distance.euclidean(xyz[l], xyz[i])
-
-            train_data_dists.append([distance_table.flatten()])
-
-            for i in range(numc) :
-                temp = data[idx + 3 + i + numxyz].split()
-                if len(temp) == 2 :
-                    atmcount = temp[0]
-                    n = len(atmcount)
-                    connect[i][0] = int(atmcount[0:n/2])
-                    connect[i][1] = int(atmcount[n/2:n])
-                    connect[i][2] = int(temp[1])
-                else :
-                    connect[i][0] = int(temp[0])
-                    connect[i][1] = int(temp[1])
-                    connect[i][2] = int(temp[2])
-            # Record connection information in the connection table of all Atoms in molecule
-            for i in range(numc):
-                connection_table[connect[i][0] - 1][connect[i][1] - 1] = 1.0
-                connection_table[connect[i][1] - 1][connect[i][0] - 1] = 1.0
-
-#            atmnum.shape = (28, 1)
-#            connection_table_final = np.hstack((atmnum, connection_table))
-            train_data_connects.append([connection_table.flatten()])
-
-    return train_data_dists, train_data_connects
+import sdfparse as p
 
 # x will be the input matrix flattened (28x29)
 x = tf.placeholder(tf.float32, [None, 812])
@@ -121,7 +42,7 @@ train_step = tf.train.AdamOptimizer(0.001).minimize(cross_entropy)
 sess = tf.InteractiveSession()
 tf.global_variables_initializer().run()
 
-a, b = get_batch()
+a, b = p.get_batch()
 train_len = len(a)
 
 correct_prediction = tf.equal(y_, y)
